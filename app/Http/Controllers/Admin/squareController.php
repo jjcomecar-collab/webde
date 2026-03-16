c07124c986a888feb8532b884fe6ecc6c91b57a1
joel <jjcomecar@unitru.edu.pe>
2026-03-15 20:39:32 -0500

    forEFWFEFFEFEn

--- app/Http/Controllers/Admin/squareController.php
+++ app/Http/Controllers/Admin/squareController.php
@@ -10,12 +10,18 @@ use Illuminate\Http\Request;
 class squareController extends Controller
 {
 
-public function index(Request $request)
-{
-    $squares = Tablesquare::orderBy('id','asc')->get();
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $squares = Tablesquare::orderBy('id','asc')->get();
 
-    dd($squares);
-}
+            return response()->json([
+                'data' => $squares
+            ]);
+        }
+
+        return view('admin.square.index');
+    }
 
 
 
