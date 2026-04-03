1208fa099e9ab37279447deacdfe2b4fa729fefb
joel <jjcomecar@unitru.edu.pe>
2026-04-03 10:42:19 -0500

    A_aa_a_A

--- app/Http/Controllers/Admin/TableServiceDetailController.php
+++ app/Http/Controllers/Admin/TableServiceDetailController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\TableserviceDetail;
+use App\Models\TableServiceDetail;
 use Illuminate\Http\Request;
 
 class TableServiceDetailController extends Controller
